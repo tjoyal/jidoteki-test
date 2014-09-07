59371c6b92119f5a04a853d5acb4064c62a52351
Thierry Joyal <thierry.joyal@gmail.com>
2014-09-07 07:38:59 -0400

    Update default.rb

--- cookbooks/test/recipes/default.rb
+++ cookbooks/test/recipes/default.rb
@@ -1,13 +1,13 @@
 # myapp recipe
 
-cookbook_file '/tmp/test.json' do
+cookbook_file '/home/ubuntu/test.json' do
   source 'test.json'
   owner 'root'
   group 'root'
   mode '0644'
 end
 
-template '/tmp/test.conf' do
+template '/home/ubuntu/test.conf' do
   source 'test.conf.erb'
   owner 'root'
   group 'root'
